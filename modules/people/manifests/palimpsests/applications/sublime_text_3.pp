c74fe98783ec8b9502624def4c2781c2c1b73da6
palimpsests <zach@monsoonco.com>
2014-05-14 16:08:50 -0700

    st3

--- modules/people/manifests/palimpsests/applications/sublime_text_3.pp
+++ modules/people/manifests/palimpsests/applications/sublime_text_3.pp
@@ -1,15 +1,15 @@
-class people::palimpsests::applications::sublime_text_2 (
+class people::palimpsests::applications::sublime_text_3 (
   $my_sourcedir   = $people::palimpsests::params::my_sourcedir,
   $my_homedir     = $people::palimpsests::params::my_homedir,
   $my_username    = $people::palimpsests::params::my_username
 ) {
 
-  notify { 'class people::palimpsests::applications::sublime_text_2 declared': }
+  notify { 'class people::palimpsests::applications::sublime_text_3 declared': }
 
-  include sublime_text_2
+  include sublime_text_3
 
   define addpkg {
-    $packagedir = "Library/Application Support/Sublime Text 2/Packages/"
+    $packagedir = "Library/Application Support/Sublime Text 3/Packages/"
     $pkgarray = split($name, '[/]')
     $pkgname = $pkgarray[1]
 
@@ -18,21 +18,21 @@ class people::palimpsests::applications::sublime_text_2 (
       provider => 'shell',
       creates  => "/Users/${::luser}/${packagedir}${pkgname}",
       path     => "${boxen::config::homebrewdir}/bin",
-      require  => [Package['SublimeText2'], Class['git']],
+      require  => [Package['SublimeText3'], Class['git']],
     }
   }
 
   $base = "/Users/${::luser}/Library/Application Support"
   file { [
-      "${base}/Sublime Text 2",
-      "${base}/Sublime Text 2/Packages",
+      "${base}/Sublime Text 3",
+      "${base}/Sublime Text 3/Packages",
     ]:
     ensure => "directory"
   }->
 
-  file { "${base}/Sublime Text 2/Packages/User":
+  file { "${base}/Sublime Text 3/Packages/User":
     ensure  => link,
-    target  => "${my_sourcedir}/dotfiles/sublime-text-2/Packages/User",
+    target  => "${my_sourcedir}/dotfiles/sublime-text-3/Packages/User",
     require => Repository["dotfiles"],
   }
 
