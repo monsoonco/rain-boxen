80ce79d3a268ef9257ef08d24a89994501424ac6
palimpsests <zach@monsoonco.com>
2014-05-14 14:52:11 -0700

    adds updated dependencies

--- modules/people/manifests/palimpsests/applications/main.pp
+++ modules/people/manifests/palimpsests/applications/main.pp
@@ -2,37 +2,37 @@ class people::palimpsests::applications::main {
 
   notify { 'class people::palimpsests::applications::main declared': }
 
-  # include clojure
+  include clojure
   # include csshx
   # include ctags
   include mongodb
   include mysql
   include phantomjs
   include postgresql
-  include tmux
+  # include tmux
   include zsh
 
   include alfred2
-  include appcleaner
-  include clipmenu
-  include colloquy
+  # include appcleaner
+  # include clipmenu
+  # include colloquy
   include chrome::canary
-  include cyberduck
+  # include cyberduck
   include emacs
-  include fluid
+  # include fluid
   include flux
   include iterm2::dev
-  include mou
+  # include mou
   include openoffice
   include rdio
-  include sequel_pro
-  include skype
-  include slate
+  # include sequel_pro
+  # include skype
+  # include slate
   include transmission
-  include tunnelblick
+  # include tunnelblick
   include virtualbox
   include vlc
-  include vmware_fusion
+  # include vmware_fusion
 
 
   # Homebrew Packages
