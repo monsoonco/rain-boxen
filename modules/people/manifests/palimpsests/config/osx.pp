5f0b475db8dc32f028a5f2ca9c6c535eec1c32bf
palimpsests <zach@monsoonco.com>
2014-05-14 16:07:33 -0700

    this

--- modules/people/manifests/palimpsests/config/osx.pp
+++ modules/people/manifests/palimpsests/config/osx.pp
@@ -11,21 +11,21 @@ class people::palimpsests::config::osx {
   }
 
   # include osx::animations
-  include osx::dashboard
-  include osx::desktop
-  include osx::disable_gatekeeper
-  include osx::disable_naturalscroll
-  include osx::diskutility
-  include osx::dock
-  include osx::finder
-  include osx::keyboard
-  include osx::missioncontrol
-  include osx::mouse
-  include osx::safari
-  include osx::screen
-  include osx::system
-  include osx::terminal
-  include osx::textedit
-  include osx::timemachine
+  # include osx::dashboard
+  # include osx::desktop
+  # include osx::disable_gatekeeper
+  # include osx::disable_naturalscroll
+  # include osx::diskutility
+  # include osx::dock
+  # include osx::finder
+  # include osx::keyboard
+  # include osx::missioncontrol
+  # include osx::mouse
+  # include osx::safari
+  # include osx::screen
+  # include osx::system
+  # include osx::terminal
+  # include osx::textedit
+  # include osx::timemachine
 
 }
