95aced67f50bac869017218bbe5d1a18ae4b6b4c
palimpsests <zach@monsoonco.com>
2014-05-14 16:11:44 -0700

    removed st3 for now

--- modules/people/manifests/palimpsests.pp
+++ modules/people/manifests/palimpsests.pp
@@ -6,7 +6,7 @@ class people::palimpsests {
   include people::palimpsests::applications::main
   include people::palimpsests::applications::gems
   include people::palimpsests::applications::npm
-  include people::palimpsests::applications::sublime_text_3
+  # include people::palimpsests::applications::sublime_text_3
 
   include people::palimpsests::config::dock
   include people::palimpsests::config::dotfiles
